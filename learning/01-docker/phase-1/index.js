0ecb6e89edbfc027f2fdb5d4d2b16c6226f77988
Abhishek <varmaabhishek261@gmail.com>
2026-06-20 11:11:59 +0530

    Dockerised Node.js application

---
+++ learning/01-docker/phase-1/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get("/",(req,res)=>{
+    return res.status(200).json({message:"Hello from Docker"})
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
